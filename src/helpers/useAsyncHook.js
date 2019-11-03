c846af9d289700305921825ef4126ebc0fa0ac8b
Konstantin <picprofit@gmail.com>
2019-11-03 16:24:17 +0300

    add:loading status to async hook

--- src/helpers/useAsyncHook.js
+++ src/helpers/useAsyncHook.js
@@ -1,4 +1,5 @@
 import {useState, useEffect} from 'react';
+import NProgress from 'nprogress';
 
 const useAsyncHook = link => {
   const [result, setResult] = useState([]);
@@ -9,12 +10,14 @@ const useAsyncHook = link => {
       try {
         if(!loading && result.length === 0) {
           console.log('loading true');
+          NProgress.start();
           setLoading(true);
           const response = await fetch(link);
           const json = await response.json();
           setResult(json);
           console.log('loading false');
           setLoading(false);
+          NProgress.done();
         }
       } catch (error) {
         setLoading(false);
