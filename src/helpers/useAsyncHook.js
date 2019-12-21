4abdc58d0ac42973f33e082bc5cba9b6f6473322
Konstantin <picprofit@gmail.com>
2019-12-21 13:00:00 +0300

    add:async hook helper

--- src/helpers/useAsyncHook.js
+++ src/helpers/useAsyncHook.js
@@ -1,21 +1,18 @@
 import { useState, useEffect } from 'react';
 import NProgress from 'nprogress';
 
-const useAsyncHook = link => {
+const useAsyncHook = ({ link }) => {
   const [result, setResult] = useState([]);
   const [loading, setLoading] = useState(false);
-  console.log('hook run');
   useEffect(() => {
     (async () => {
       try {
         if (!loading && result.length === 0) {
-          console.log('loading true');
           NProgress.start();
           setLoading(true);
           const response = await fetch(link);
           const json = await response.json();
           setResult(json);
-          console.log('loading false');
           setLoading(false);
           NProgress.done();
         }
