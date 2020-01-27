e8410cfbeca6fe3618d04e8221ffff6f82ab86d8
Konstantin <picprofit@gmail.com>
2020-01-27 18:01:45 +0300

    upd: Profile data from remote

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NProgress from 'nprogress';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import parse from 'html-react-parser';
 
 import { personalDataLink } from '../../config';
 import useAsyncHook from '../../helpers/useAsyncHook';
@@ -10,6 +11,7 @@ import Skills from '../Skills';
 import PersonalInfo from '../PersonalInfo';
 
 const Profile = () => {
+  const history = useHistory();
   NProgress.start();
   const [CVData, loading] = useAsyncHook({ link: personalDataLink });
 
@@ -24,6 +26,13 @@ const Profile = () => {
 
   const { data } = CVData;
 
+  const contentClickHandler = e => {
+    const targetLink = e.target.closest('a');
+    if (!targetLink) return;
+    e.preventDefault();
+    history.push(e.target.getAttribute("href"));
+  };
+
   return (
     <>
       <h1>
@@ -33,23 +42,10 @@ const Profile = () => {
         </b>
         <span>{data.position}</span>
       </h1>
-      <p>
-        I&apos;m looking for <b>remote React frontend developer</b> position.
-      </p>
-      <p>
-        I&apos;m new in React, but I have a good experience in Web Development.
-        I work as fullstack developer using mainly PHP/JS since 2010 and
-        WordPress since 2015, and now (since 2019) I want to work more with a
-        frontend and React.
-      </p>
-      <p>
-        For now I created a couple of apps in React, you can see them in{' '}
-        <Link to="/portfolio">my portfolio</Link>.
-      </p>
-      <p>
-        Looking for job to expand my opportunities, not only to exchange my time
-        for money.
-      </p>
+      {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */}
+      <div onClick={contentClickHandler}>
+        {CVData.description ? parse(CVData.description) : null}
+      </div>
       <Skills />
       <PersonalInfo />
     </>
