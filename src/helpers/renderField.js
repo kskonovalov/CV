82078d9c19acb95942daa0fbfa77c66c398c40c6
Konstantin <picprofit@gmail.com>
2020-01-28 11:04:49 +0300

    upd: renderField moved to helpers

---
+++ src/helpers/renderField.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const renderField = field => {
+  const { blank, link, name, value } = field;
+  const target = blank ? '_blank' : '_self';
+  return link !== undefined ? (
+    <div className="personal-item" key={name}>
+      <em>{name}:</em>
+      <span>
+        <a href={link} target={target}>
+          {value}
+        </a>
+      </span>
+    </div>
+  ) : (
+    <div className="personal-item" key={name}>
+      <em>{name}:</em>
+      <span>{value}</span>
+    </div>
+  );
+};
+
+export default renderField;
