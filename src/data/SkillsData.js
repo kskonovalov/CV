db973f76202e494dcb5c7f10f1e2462981425060
Konstantin <picprofit@gmail.com>
2020-03-15 16:12:25 +0300

    upd: skills

--- src/data/SkillsData.js
+++ src/data/SkillsData.js
@@ -1,46 +1,54 @@
 const SkillsData = {
   Main: [
     {
-      title: 'React',
-      since: 'since 2018, last use: 2019'
+      title: 'React (hooks)',
+      since: 'since 2018, last use: 2020'
     },
     {
-      title: 'Redux',
-      since: 'since 2019, last use: 2019'
+      title: 'JavaScript (ES6)',
+      since: 'since 2015, last use: 2020'
     },
     {
-      title: 'JavaScript, ES6',
-      since: 'since 2015, last use: 2019'
+      title: 'TypeScript',
+      since: 'since 2020, last use: 2020'
+    },
+    {
+      title: 'Redux (thunk, saga)',
+      since: 'since 2019, last use: 2020'
+    },
+    {
+      title: 'GraphQL',
+      since: 'since 2019, last use: 2020'
     },
     {
       title: 'HTML, HTML5',
-      since: 'since 2010, last use: 2019'
+      since: 'since 2010, last use: 2020'
     },
     {
       title: 'CSS, CSS3',
-      since: 'since 2010, last use: 2019'
+      since: 'since 2010, last use: 2020'
     },
     {
       title: 'Less, Sass/SCSS',
-      since: 'since 2019, last use: 2019'
+      since: 'since 2019, last use: 2020'
     },
     {
       title: 'Git',
-      since: 'since 2015, last use: 2019'
+      since: 'since 2015, last use: 2020'
     }
   ],
   Also: [
     {
       title: 'WordPress',
-      since: 'since 2013, last use: 2019'
+      since: 'since 2015, last use: 2020'
     },
     {
       title: 'PHP',
-      since: 'since 2010, last use: 2019'
+      since: 'since 2010, last use: 2020'
     },
     {
       title: 'MySQL',
-      since: 'since 2011, last use: 2019'
+      since: 'since 2011, last use: 2020'
     }
   ]
 };
